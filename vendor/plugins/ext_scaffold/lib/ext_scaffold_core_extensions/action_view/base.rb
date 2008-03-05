2a0690c8141ebe3a19cc871fd2a8dafee9712515
Dru Nelson <drudru@gmail.com>
2008-03-04 23:11:18 -0800

    Change date format

--- vendor/plugins/ext_scaffold/lib/ext_scaffold_core_extensions/action_view/base.rb
+++ vendor/plugins/ext_scaffold/lib/ext_scaffold_core_extensions/action_view/base.rb
@@ -192,7 +192,7 @@ module ExtScaffoldCoreExtensions
         js << "  allowBlank: #{options[:allow_blank] == false ? 'false' : 'true'}," unless options[:allow_blank].nil?
         js << "  vtype: '#{options[:vtype]}'," if options[:vtype]
         js << "  xtype: '#{options[:xtype]}'," if options[:xtype]
-        js << "  format: 'Y/m/d'," if options[:xtype] == 'datefield'
+        js << "  format: 'm/d/Y'," if options[:xtype] == 'datefield'
         js << "  dateFormat: 'Y/m/d', timeFormat: 'H:i:s'," if options[:xtype] == 'xdatetime'
         js << "  inputValue: '1', width: 18, height: 21," if options[:xtype] == 'checkbox'
         js << "  name: '#{options[:name]}'"
