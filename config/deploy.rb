75cc5e34986cf66a4bb71689f3438af003799678
Dru Nelson <dru@ubuntu.localdomain>
2008-03-29 04:13:49 -0700

    Yet another hopeful tweak to cap

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,23 +2,22 @@
 require 'mongrel_cluster/recipes'
 
 set :application, "enroll"
+set :deploy_to, "/home/dru/www/#{application}"
+
+set :mongrel_conf, "#{current_path}/config/mongrel_cluster.yml"
 
 default_run_options[:pty] = true
 set :repository,  "git://github.com/drudru/enroll.git"
 set :scm, "git"
 set :user, "dru"
 set :branch, "origin/master"
+ssh_options[:port] = 73
 
 
-set :mongrel_conf, "#{current_path}/config/mongrel_cluster.yml"
 # If you aren't deploying to /u/apps/#{application} on the target
 # servers (which is the default), you can specify the actual location
 # via the :deploy_to variable:
-set :deploy_to, "/home/dru/www/#{application}"
 set :deploy_via, :remote_cache
-
-ssh_options[:port] = 73
-
 
 role :app, ENV['DEPLOY_TO']
 role :web, ENV['DEPLOY_TO']
