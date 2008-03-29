a2d586b5692a4512dd0fecae3b54e02e29cd4518
Dru Nelson <dru@ubuntu.localdomain>
2008-03-29 04:04:29 -0700

    First attempt at cap for this app

---
+++ config/deploy.rb
@@ -0,0 +1,23 @@
+
+require 'mongrel_cluster/recipes'
+
+set :application, "enroll"
+
+default_run_options[:pty] = true
+set :repository,  "git@github.com:drudru/enroll.git"
+set :scm, "git"
+set :user, "dru"
+set :branch, "origin/master"
+
+
+set :mongrel_conf, "#{current_path}/config/mongrel_cluster.yml"
+# If you aren't deploying to /u/apps/#{application} on the target
+# servers (which is the default), you can specify the actual location
+# via the :deploy_to variable:
+set :deploy_to, "/home/dru/www/#{application}"
+set :deploy_via, :remote_cache
+
+role :app, ENV['DEPLOY_TO']
+role :web, ENV['DEPLOY_TO']
+role :db,  ENV['DEPLOY_TO'], :primary => true
+
