1b1ed1284cf3ae0d565805435523afaa01e858e6
Dru Nelson <dru@ubuntu.localdomain>
2008-03-29 04:10:31 -0700

    More deploy tweaks

--- config/deploy.rb
+++ config/deploy.rb
@@ -4,7 +4,7 @@ require 'mongrel_cluster/recipes'
 set :application, "enroll"
 
 default_run_options[:pty] = true
-set :repository,  "git@github.com:drudru/enroll.git"
+set :repository,  "git://github.com/drudru/enroll.git"
 set :scm, "git"
 set :user, "dru"
 set :branch, "origin/master"
@@ -16,6 +16,9 @@ set :mongrel_conf, "#{current_path}/config/mongrel_cluster.yml"
 # via the :deploy_to variable:
 set :deploy_to, "/home/dru/www/#{application}"
 set :deploy_via, :remote_cache
+
+ssh_options[:port] = 73
+
 
 role :app, ENV['DEPLOY_TO']
 role :web, ENV['DEPLOY_TO']
